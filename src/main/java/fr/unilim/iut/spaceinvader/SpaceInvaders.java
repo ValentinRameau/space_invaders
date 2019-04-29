5a7f0e3e2a6ae2faf1465aa74f635a0fff4d4aed
valentin rameau <valentin.rameau@etu.unilim.fr>
2019-04-29 08:43:36 +0200

    Changement du toString en recupererEspaceJeuDansChaineASCII.

--- src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
@@ -1,9 +1,14 @@
 package fr.unilim.iut.spaceinvader;
   
-    public class SpaceInvaders {
+	public class SpaceInvaders {
 
-	    int longueur;
-	    int hauteur;
+		private static final char MARQUE_FIN_LIGNE = '\n';
+		private static final char MARQUE_VIDE = '.';
+		private static final char MARQUE_VAISSEAU = 'V';
+		int longueur;
+		int hauteur;
+		Vaisseau vaisseau;
+
 
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
@@ -12,14 +17,39 @@ package fr.unilim.iut.spaceinvader;
 	    
 	    @Override
 		public String toString() {
+			return recupererEspaceJeuDansChaineASCII();
+		}
+
+		private String recupererEspaceJeuDansChaineASCII() {
 			StringBuilder espaceDeJeu = new StringBuilder();
-			for (int i = 0; i < hauteur; i++) {
-				for (int j = 0; j < longueur; j++) {
-					espaceDeJeu.append('.');
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+				    char marque = recupererMarqueDeLaPosition(x, y);			
+				    espaceDeJeu.append(marque);
 				}
-				espaceDeJeu.append('\n');
+				espaceDeJeu.append(MARQUE_FIN_LIGNE);
 			}
 			return espaceDeJeu.toString();
 		}
 
+		private char recupererMarqueDeLaPosition(int x, int y) {
+			char marque;
+			if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+			      marque=MARQUE_VAISSEAU;
+			else
+			      marque=MARQUE_VIDE;
+			return marque;
+		}
+
+		private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+			return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+		}
+
+		private boolean aUnVaisseau() {
+			return vaisseau!=null;
+		}
+
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+	        this.vaisseau = new Vaisseau(x, y);
+		}
    }
